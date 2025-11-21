6b34559468ebba3bb58b566f6810b52de4e53b3b
EdrianSegundo <24-08542@g.batstate-u.edu.ph>
2025-11-21 16:16:30 +0800

    added, simple login, user file, home after login

--- tamadgo.py
+++ tamadgo.py
@@ -1,11 +1,29 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
+import json
+
 app = Flask(__name__)
+
+def load_users():
+    with open("users.json", "r") as file:
+        return json.load(file)
 
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def index():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    users = load_users()
+
+    if username in users and users[username]["password"] == password:
+        return render_template("home.html", user=username)
+    else:
+        return "<h2>Invalid username or password</h2><a href='/'>Try again</a>"
 
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
